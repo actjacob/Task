64faf808e94e379350c60a05604a4871fcff3b41
actjacob <yakupaktar99@gmail.com>
2025-01-12 00:23:27 +0300

    Fixed:fix the redux and all issiues

--- app/store.js
+++ app/store.js
@@ -1,9 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from './store/authSlice';
 
-export const store =
-  configureStore(
-    {
-      reducer:
-        {},
-    }
-  );
+export const store = configureStore({
+   reducer: {
+      auth: authSlice,
+   },
+});
