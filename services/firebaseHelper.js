ae83464d7e5dc32b219cb3609f9338a3271efe01
actjacob <yakupaktar99@gmail.com>
2025-02-21 04:27:40 +0300

    Feature:Firebase configuration to be managed via the .env file

--- services/firebaseHelper.js
+++ services/firebaseHelper.js
@@ -1,34 +1,59 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { Platform } from 'react-native';
+import {
+   FIREBASE_WEB_API_KEY,
+   FIREBASE_WEB_AUTH_DOMAIN,
+   FIREBASE_WEB_PROJECT_ID,
+   FIREBASE_WEB_STORAGE_BUCKET,
+   FIREBASE_WEB_MESSAGING_SENDER_ID,
+   FIREBASE_WEB_APP_ID,
+   FIREBASE_WEB_MEASUREMENT_ID,
+   FIREBASE_WEB_DATABASE_URL,
+   FIREBASE_ANDROID_API_KEY,
+   FIREBASE_ANDROID_APP_ID,
+   FIREBASE_ANDROID_DATABASE_URL,
+} from '@env';
 
 export const getFirebaseApp = () => {
    console.log('Firebase başlatıldı!');
-   // https://firebase.google.com/docs/web/setup#available-libraries
+   console.log('FIREBASE_WEB_API_KEY:', FIREBASE_WEB_API_KEY);
+   console.log('FIREBASE_ANDROID_API_KEY:', FIREBASE_ANDROID_API_KEY);
 
-   const firebaseWebConfig = {
-      apiKey: 'AIzaSyBGdhzpLOHPubv-4YD2QxV29kJHDjC766Y',
-      authDomain: 'istakip-87ae2.firebaseapp.com',
-      projectId: 'istakip-87ae2',
-      storageBucket: 'istakip-87ae2.firebasestorage.app',
-      messagingSenderId: '78398604874',
-      appId: '1:78398604874:web:aa17672c678629d3f388b3',
-      measurementId: 'G-NWS88BKEC8',
+   const firebaseConfig = {
+      apiKey: Platform.OS === 'web' ? FIREBASE_WEB_API_KEY : FIREBASE_ANDROID_API_KEY,
+      authDomain: FIREBASE_WEB_AUTH_DOMAIN,
+      projectId: FIREBASE_WEB_PROJECT_ID,
+      storageBucket: FIREBASE_WEB_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_WEB_MESSAGING_SENDER_ID,
+      appId: Platform.OS === 'web' ? FIREBASE_WEB_APP_ID : FIREBASE_ANDROID_APP_ID,
+      measurementId: FIREBASE_WEB_MEASUREMENT_ID,
+      databaseURL:
+         Platform.OS === 'web'
+            ? FIREBASE_WEB_DATABASE_URL
+            : FIREBASE_ANDROID_DATABASE_URL,
    };
-
-   const firebaseAndroidConfig = {
-      apiKey: 'AIzaSyBfUC1Vs8vxNUF2AnY5UpF2TV6azCUuZ5w',
-      authDomain: 'istakip-87ae2.firebaseapp.com',
-      projectId: 'istakip-87ae2',
-      storageBucket: 'istakip-87ae2.firebasestorage.app',
-      messagingSenderId: '78398604874',
-      appId: '1:78398604874:android:0a1f33097235d5b8f388b3',
-   };
-
-   const firebaseConfig =
-      Platform.OS === 'web' ? firebaseWebConfig : firebaseAndroidConfig;
 
    const app = initializeApp(firebaseConfig);
 
    return app;
 };
+
+// const firebaseConfig = {
+//    apiKey:
+//       Platform.OS === 'web'
+//          ? Config.FIREBASE_WEB_API_KEY
+//          : Config.FIREBASE_ANDROID_API_KEY,
+//    authDomain: Config.FIREBASE_WEB_AUTH_DOMAIN,
+//    projectId: Config.FIREBASE_WEB_PROJECT_ID,
+//    storageBucket: Config.FIREBASE_WEB_STORAGE_BUCKET,
+//    messagingSenderId: Config.FIREBASE_WEB_MESSAGING_SENDER_ID,
+//    appId:
+//       Platform.OS === 'web'
+//          ? Config.FIREBASE_WEB_APP_ID
+//          : Config.FIREBASE_ANDROID_APP_ID,
+//    measurementId: Config.FIREBASE_WEB_MEASUREMENT_ID,
+//    databaseURL:
+//       Platform.OS === 'web'
+//          ? Config.FIREBASE_WEB_DATABASE_URL
+//          : Config.FIREBASE_ANDROID_DATABASE_URL,
+// };
